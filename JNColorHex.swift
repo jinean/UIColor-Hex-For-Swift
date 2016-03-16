2ff95840242e24bda8ee7a86135ce187378da12f
liyan <liyan@liyandeMacBook-Pro.local>
2016-03-16 17:43:20 +0800

    1.0.0

--- JNColorHex.swift
+++ JNColorHex.swift
@@ -10,7 +10,7 @@ extension UIColor
 {
     class func getColor(hexColor: String) -> UIColor? {
         
-        if (distance(hexColor.startIndex,hexColor.endIndex) < 6 )
+        if (hexColor.startIndex.distanceTo(hexColor.endIndex) < 6 )
         {
             return nil
         }
@@ -19,24 +19,24 @@ extension UIColor
         var greenInt_: UInt32   = 0
         var blueInt_: UInt32    = 0
         
-        var startIndex  = advance(hexColor.startIndex, 0)
-        var endIndex    = advance(startIndex, 2);
+        var startIndex  = hexColor.startIndex.advancedBy(0)
+        var endIndex    = startIndex.advancedBy(2)
         
         var rangeNSRange_: Range = Range<String.Index>(start: startIndex, end: endIndex)
         NSScanner.localizedScannerWithString(hexColor.substringWithRange(rangeNSRange_)).scanHexInt(&redInt_)
         
-        startIndex      = advance(hexColor.startIndex, 2)
-        endIndex        = advance(startIndex, 2);
+        startIndex      = hexColor.startIndex.advancedBy(2)
+        endIndex        = startIndex.advancedBy(2)
         
         rangeNSRange_   = Range<String.Index>(start: startIndex, end: endIndex)
         NSScanner.localizedScannerWithString(hexColor.substringWithRange(rangeNSRange_)).scanHexInt(&greenInt_)
         
-        startIndex      = advance(hexColor.startIndex, 4)
-        endIndex        = advance(startIndex, 2);
+        startIndex      = hexColor.startIndex.advancedBy(4)
+        endIndex        = startIndex.advancedBy(2)
         
         rangeNSRange_   = Range<String.Index>(start: startIndex, end: endIndex)
         NSScanner.localizedScannerWithString(hexColor.substringWithRange(rangeNSRange_)).scanHexInt(&blueInt_)
-
+        
         return UIColor(red: CGFloat(redInt_) / 255.0, green: CGFloat(greenInt_) / 255.0, blue: CGFloat(blueInt_) / 255.0, alpha: 1.0)
 
     }
